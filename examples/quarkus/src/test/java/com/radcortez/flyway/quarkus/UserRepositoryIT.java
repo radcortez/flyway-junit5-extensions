a015afa7452f1e53b0d9424f6b8e33eb84847b4e
Roberto Cortez <radcortez@yahoo.com>
2021-06-21 13:38:02 +0100

    Add native image test.

---
+++ examples/quarkus/src/test/java/com/radcortez/flyway/quarkus/UserRepositoryIT.java
@@ -0,0 +1,19 @@
+package com.radcortez.flyway.quarkus;
+
+import com.radcortez.flyway.test.annotation.DataSource;
+import com.radcortez.flyway.test.annotation.FlywayTest;
+import com.radcortez.flyway.test.junit.DataSourceInfo;
+import com.radcortez.flyway.test.junit.DataSourceProvider;
+import io.quarkus.test.junit.NativeImageTest;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+@FlywayTest(@DataSource(UserRepositoryIT.QuarkusDataSourceProvider.class))
+@NativeImageTest
+public class UserRepositoryIT extends UserRepositoryTest {
+    public static class QuarkusDataSourceProvider implements DataSourceProvider {
+        @Override
+        public DataSourceInfo getDatasourceInfo(final ExtensionContext extensionContext) {
+            return DataSourceInfo.config("jdbc:postgresql://localhost:5432/database", "database", "password");
+        }
+    }
+}
